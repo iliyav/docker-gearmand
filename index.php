d174a3decb3cbc6e368f1672b279a14926701402
Illia Vavilin <dstrelok@gmail.com>
2015-04-09 12:40:51 +0300

    Added gearman-ui to image.

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+$filename = __DIR__.preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
+if (php_sapi_name() === 'cli-server' && is_file($filename)) {
+    return false;
+}
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new GearmanUI\GearmanUIApplication();
+
+$app->run();
